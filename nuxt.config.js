1de4a02c454e5a96927053778e8986f7f46b8670
André Van Dal <deregudegu@gmail.com>
2017-10-30 03:27:27 -0200

    add webpack monitor

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,9 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {},
+  build: {
+    analyze: true
+  },
   /*
   ** Headers
   ** Common headers are already provided by @nuxtjs/pwa preset
@@ -21,7 +23,33 @@ module.exports = {
   /*
   ** Modules
   */
+  // router: {
+  //   scrollBehavior: (to, from, savedPosition) => {
+  //     // savedPosition is only available for popstate navigations.
+  //     if (savedPosition) {
+  //       return savedPosition
+  //     } else {
+  //       let position = {}
+  //       // if no children detected
+  //       if (to.matched.length < 2) {
+  //         // scroll to the top of the page
+  //         position = { x: 0, y: 0 }
+  //       }
+  //       else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+  //         // if one of the children has scrollToTop option set to true
+  //         position = { x: 0, y: 0 }
+  //       }
+  //       // if link has anchor,  scroll to anchor by returning the selector
+  //       if (to.hash) {
+  //         position = { selector: to.hash }
+  //       }
+  //       return position
+  //     }
+  //   }
+  // },
   modules: [
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/webpackmonitor'
   ]
+
 }
